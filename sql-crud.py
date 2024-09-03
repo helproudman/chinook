488b8b73a6712fa97764e93245272345fc0b05a5
Helen Proudman <helen.proudman@students.codeinstitute.net>
2024-09-03 15:22:53 +0000

    Programmer object defined and first instance created and added and committed

--- sql-crud.py
+++ sql-crud.py
@@ -10,6 +10,17 @@ db = create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
+# create a class-based model for the "Programmer" table
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+    
+
 
 # instead of connection to the database directly, we will ask for a session
 # create a new instance of sessionmaker, then point to our engine (the db)
@@ -19,3 +30,32 @@ session = Session()
 
 # creating the database using declarative_base subclass
 base.metadata.create_all(db)
+
+# create records on our Programmer table
+ada_lovelace = Programmer(
+    first_name="Ada",
+    last_name="Lovelace",
+    gender="f",
+    nationality="British",
+    famous_for="First Programmer"
+)
+
+
+# add records to the table
+session.add(ada_lovelace)
+
+# commit session to the database
+session.commit()
+
+# query the database to find all programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep = " | "
+        
+    )
